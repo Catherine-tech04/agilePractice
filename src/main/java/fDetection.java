9dfbbf9d61ead9b4d45a867433c2dbe6d19e2c6a
Catherine Maria George <cathu.george@gmail.com>
2026-04-05 23:35:59 +0530

    new local repo

---
+++ src/main/java/fDetection.java
@@ -0,0 +1,10 @@
+public class FraudDetection{
+    public static boolean isFraud(int amount){
+        return amount > 10000;
+
+    }
+
+    public static void main(String args[]{
+        print("Fraud detection system running")
+    })
+}
